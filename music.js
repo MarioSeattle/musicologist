9bdd4cf42095b829556dd315cce7050a3f31fa06
MarioSeattle <marioseattle@live.com>
2017-08-29 10:07:35 -0700

    Retrieving songs from artist and add 5 songs to playlist

--- music.js
+++ music.js
@@ -1,13 +1,44 @@
-/**
- * Created by Mgomez on 8/29/17.
- */
 const musicInfo = [];
+const playlist = [];
+
+function findTerm(term){
+    let url = "https://itunes.apple.com/search?media=music&entity=musicTrack&term=" + term
+
+    //ajax call
+    $.getJSON(
+        url,
+        function(data, status){
+            if (status === "success"){
+                let results = data["results"];
+                let count = 0;
+                for(let i=0; i<results.length; i++){
+                    let artist = results[i]["artistName"]
+                    let track = results[i]["trackName"]
+                    let url = results[i]["trackViewUrl"]
+
+                    playlist.push({
+                        "artist": artist,
+                        "track": track,
+                        "url": url
+                    })
+
+                    count ++;
+                    if(count >= 5){
+                        break;
+                    }
+                }
+            }
+        }
+    )
+}
 
 function addSongFromField(event) {
     event.preventDefault();
 
     const info = $('#musicField').eq(0).val();
 
+    // find therm in itunes
+    findTerm(info);
     musicInfo.push(info);
     renderList();
     $('#musicField').eq(0).val('');
@@ -36,5 +67,14 @@ $('#getPlaylistBtn').click(function (event) {
     // TODO: Display a list of music.
     // You may use anything from musicInfo.
     console.log('Testing Music Call');
-});
 
+    const $results = $('.results').eq(0);
+
+    $results.empty();
+
+    for (const track of playlist) {
+        let $item = $('<li class="list-group-item">').text(track.track);
+
+        $results.append($item)
+    }
+});
